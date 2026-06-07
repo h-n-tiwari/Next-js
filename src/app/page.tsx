209d8ad7f378ca3a8de3095363f83cccaf3b4400
h-n-tiwari <pmgc590@gmail.com>
2026-06-07 15:27:20 +0530

    Link Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function About() {
-    return <h1>Welcome home!</h1>;
+    return (
+        <>
+            <h1>Welcome home!</h1>
+            <Link href="/blog">Blog</Link>
+            <Link href="/products">Products</Link>
+        </>
+    );
 }
