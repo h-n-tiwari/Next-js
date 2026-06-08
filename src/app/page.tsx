b549c61c43eeae7287a33c096770daff77c6238b
h-n-tiwari <pmgc590@gmail.com>
2026-06-08 17:45:37 +0530

    Params and searchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,9 @@ export default function About() {
             <h1>Welcome home!</h1>
             <Link href="/blog">Blog</Link>
             <Link href="/products">Products</Link>
+            <Link href="/articles/breaking-news-123?lang-en">Read in English</Link>
+            <Link href="/articles/breaking-news-123?lang-es">Read in Spanish</Link>
+            <Link href="/articles/breaking-news-123?lang-fr">Read in French</Link>
         </>
     );
 }
