5a5a9c7a5679a0ebff5a8ecadee01d9d8a795df2
h-n-tiwari <pmgc590@gmail.com>
2026-06-09 17:19:20 +0530

    Recovering From Errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,5 +1,30 @@
 "use client";
 
-export default function ErrorBoundary({ error }: { error: Error }) {
-    return <div>{error.message}</div>;
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+// Error boundaries must be Client Components in Next.js.
+export default function ErrorBoundary({
+    error,
+    reset,
+}: {
+    error: Error;
+    reset: () => void;
+}) {
+    const router = useRouter();
+
+    const reload = () => {
+        startTransition(() => {
+            router.refresh();
+            reset();
+        });
+    };
+
+    return (
+        <div>
+            <p>{error.message}</p>
+
+            <button onClick={reload}>Try Again</button>
+        </div>
+    );
 }
