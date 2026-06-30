8dca76fdda3d29de21843b131825fcfe435082e5
h-n-tiwari <pmgc590@gmail.com>
2026-06-30 15:25:56 +0530

    Reading Request Headers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,11 @@
-export async function GET() {
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+    // const requestHeaders = new Headers(request.headers);
+    // console.log(requestHeaders.get("Authorization"));
+
+    const headerList = await headers();
+    console.log(headerList.get("Authorization"));
     return new Response("Profile API data");
 }
