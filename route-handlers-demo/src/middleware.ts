96daa03f4f8e895f28fffa20f01bc3e787f44c63
h-n-tiwari <pmgc590@gmail.com>
2026-07-08 15:54:43 +0530

    Handling Cookies in Middleware

--- route-handlers-demo/src/middleware.ts
+++ route-handlers-demo/src/middleware.ts
@@ -2,11 +2,20 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+
+    const response = NextResponse.next();
+    const themePreference = request.cookies.get("theme");
+    if (!themePreference) {
+        response.cookies.set("theme", "dark");
+    }
+    
+    return response;
+    
     // return NextResponse.redirect(new URL("/", request.url));
 
-    if (request.nextUrl.pathname === "/profile") {
-        return NextResponse.rewrite(new URL("/hello", request.nextUrl));
-    }
+    // if (request.nextUrl.pathname === "/profile") {
+    //     return NextResponse.rewrite(new URL("/hello", request.nextUrl));
+    // }
 
 }
 
