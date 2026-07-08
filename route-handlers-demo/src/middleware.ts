380271ea5e7405276797e2e08045bdf7193aeade
h-n-tiwari <pmgc590@gmail.com>
2026-07-08 15:16:15 +0530

    Conditional Statements in Middleware

--- route-handlers-demo/src/middleware.ts
+++ route-handlers-demo/src/middleware.ts
@@ -2,11 +2,15 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-    return NextResponse.redirect(new URL("/", request.url));
+    // return NextResponse.redirect(new URL("/", request.url));
+
+    if (request.nextUrl.pathname === "/profile") {
+        return NextResponse.redirect(new URL("/hello", request.nextUrl));
+    }
 
 }
 
-export const config = {
-    matcher: "/profile",
-}
+// export const config = {
+//     matcher: "/profile",
+// }
 
