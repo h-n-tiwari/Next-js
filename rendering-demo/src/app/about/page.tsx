d9db47092f467986f8718adc6788766bc289014b
h-n-tiwari <pmgc590@gmail.com>
2026-07-10 15:01:07 +0530

    Removing Server Components

--- rendering-demo/src/app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export default function AboutPage() {
-    const [name, setName] = useState("");
-    console.log("About Server Components");
-    return <h1>About Page</h1>
-}
