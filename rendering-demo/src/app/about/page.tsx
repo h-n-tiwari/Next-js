dd3473a320159889b6e0d8f9a4cc4832e7d2cec5
h-n-tiwari <pmgc590@gmail.com>
2026-07-13 15:41:29 +0530

    Static Rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,7 +1,16 @@
-import { useState } from "react";
+// import { useState } from "react";
+
+// export default function AboutPage() {
+//     const [name, setName] = useState("");
+//     console.log("About Server Components");
+//     return <h1>About Page</h1>
+// }
+
+
+// STATIC RENDERING
 
 export default function AboutPage() {
-    const [name, setName] = useState("");
     console.log("About Server Components");
-    return <h1>About Page</h1>
+    return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
 }
+
