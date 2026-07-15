69519397fd5e6800a5cca63717ae27fbaa2c67e6
h-n-tiwari <pmgc590@gmail.com>
2026-07-15 20:38:34 +0530

    Using Cookies for Dynamic Rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -9,7 +9,21 @@
 
 // STATIC RENDERING
 
-export default function AboutPage() {
+// export default function AboutPage() {
+//     console.log("About Server Components");
+//     return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
+// }
+
+
+// DYNAMIC RENDERING
+
+import { cookies } from "next/headers";
+
+export default async function AboutPage() {
+    const cookieStore = await cookies();
+    const theme = cookieStore.get("theme");
+    console.log(theme);
+    
     console.log("About Server Components");
     return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
 }
