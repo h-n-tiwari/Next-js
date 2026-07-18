537cdb2be5e86804a945d45f5369a79e6835b9bb
h-n-tiwari <pmgc590@gmail.com>
2026-07-18 12:15:29 +0530

    Streaming - Setting Up Suspense Boundaries

---
+++ rendering-demo/src/app/product-reviews/page.tsx
@@ -0,0 +1,12 @@
+import { Product } from "@/components/product";
+import { Reviews } from "@/components/reviews";
+
+export default function ProductReviews() {
+    return (
+        <div>
+            <h1>Product Reviews</h1>
+            <Product />
+            <Reviews />
+        </div>
+    );
+}
---
+++ rendering-demo/src/components/reviews.tsx
@@ -0,0 +1,4 @@
+export const Reviews = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return <div>Reviews</div>;
+};
