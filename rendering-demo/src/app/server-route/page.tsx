121311ee39e34921118fd5f3ce1f2b3afd887b74
h-n-tiwari <pmgc590@gmail.com>
2026-07-21 15:34:28 +0530

    Encapsulating Client Features in Server Components

--- rendering-demo/src/app/server-route/page.tsx
+++ rendering-demo/src/app/server-route/page.tsx
@@ -8,31 +8,17 @@
 
 // THIRD PARTY PACKAGES
 
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { serverSideFunction } from "@/utils/server-utils";
 
-export default function ClientRoutePage() {
-    const settings = {
-        dots: true,
-    };
+import { ImageSlider } from "@/components/ImageSlider";
+
+
+export default function ServerRoutePage() {
+    const result = serverSideFunction();
     return (
-        <div className="image-slider-container">
-            <Slider {...settings}>
-                <div>
-                    <img src="https://picsum.photos/400/200" />
-                </div>
-                <div>
-                    <img src="https://picsum.photos/400/200" />
-                </div>
-                <div>
-                    <img src="https://picsum.photos/400/200" />
-                </div>
-                <div>
-                    <img src="https://picsum.photos/400/200" />
-                </div>
-            </Slider>
-        </div>
+        <>
+            <h1>Server route {result}</h1>
+            <ImageSlider />
+        </> 
     );
 }
