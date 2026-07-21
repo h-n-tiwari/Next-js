2ac40bddc5fb5ef374a479b5b6f2ce7c324386d5
h-n-tiwari <pmgc590@gmail.com>
2026-07-21 20:37:24 +0530

    Implementing Client-Only Function in Components

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -59,3 +59,23 @@
 
 
 
+// CLIENT-ONLY CODE
+
+"use client";
+
+import { useTheme } from "@/components/theme-provider";
+import { clientSideFunction } from "@/utils/client-utils";
+
+import React from "react";
+
+export default function ClientRoutePage() {
+	const theme = useTheme();
+	const result = clientSideFunction();
+
+	return (
+		<>
+			<h1 style={{ color: theme.colors.primary }}>Client router page</h1>
+			<p>{result}</p>
+		</>
+	)
+}
