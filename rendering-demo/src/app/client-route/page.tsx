df7e12586b816df6e6162fa04ccdaff5a35eb0d9
h-n-tiwari <pmgc590@gmail.com>
2026-07-21 16:40:50 +0530

    Testing Theme Context in Client Route Page

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -9,34 +9,50 @@
 
 // THIRD PARTY PACKAGES
 
+// "use client";
+
+// import React from "react";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
+
+// export default function ClientRoutePage() {
+//     const settings = {
+//       dots: true,
+//     };
+//     return (
+
+//         <div className="image-slider-container">
+//           <Slider {...settings}>
+//             <div>
+//               <img src="https://picsum.photos/400/200" />
+//             </div>
+//             <div>
+//               <img src="https://picsum.photos/400/200" />
+//             </div>
+//             <div>
+//               <img src="https://picsum.photos/400/200" />
+//             </div>
+//             <div>
+//               <img src="https://picsum.photos/400/200" />
+//             </div>
+//           </Slider>
+//         </div>
+//     );
+// }
+
+
+// CONTEXT PROVIDERS
+
 "use client";
 
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { useTheme } from "@/components/theme-provider";
 
+import React from "react";
 
 export default function ClientRoutePage() {
-    const settings = {
-      dots: true,
-    };
-    return (
-        <div className="image-slider-container">
-          <Slider {...settings}>
-            <div>
-              <img src="https://picsum.photos/400/200" />
-            </div>
-            <div>
-              <img src="https://picsum.photos/400/200" />
-            </div>
-            <div>
-              <img src="https://picsum.photos/400/200" />
-            </div>
-            <div>
-              <img src="https://picsum.photos/400/200" />
-            </div>
-          </Slider>
-        </div>
-    );
+	const theme = useTheme();
+
+	return <h1 style={{ color: theme.colors.primary }}>Client router page</h1>
 }
