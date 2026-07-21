23c853ac83d660d6083c5ed0a29a5a1e2fa46850
h-n-tiwari <pmgc590@gmail.com>
2026-07-21 20:45:47 +0530

    Safeguarding Client-Only Code

--- rendering-demo/src/app/server-route/page.tsx
+++ rendering-demo/src/app/server-route/page.tsx
@@ -6,18 +6,42 @@
 
 // }
 
-// THIRD PARTY PACKAGES
+// // THIRD PARTY PACKAGES
+
+// import { serverSideFunction } from "@/utils/server-utils";
+
+// import { ImageSlider } from "@/components/ImageSlider";
+
+
+// export default function ServerRoutePage() {
+//     const result = serverSideFunction();
+//     return (
+//         <>
+//             <h1>Server route {result}</h1>
+//             <ImageSlider />
+//         </> 
+//     );
+// }
+
+
+
+// CLIENT-ONLY CODE
+
 
 import { serverSideFunction } from "@/utils/server-utils";
 
 import { ImageSlider } from "@/components/ImageSlider";
+import { clientSideFunction } from "@/utils/client-utils";
+
 
 
 export default function ServerRoutePage() {
     const result = serverSideFunction();
+        const clientResult = clientSideFunction();
+    
     return (
         <>
-            <h1>Server route {result}</h1>
+            <h1>Server route {result} {clientResult}</h1>
             <ImageSlider />
         </> 
     );
