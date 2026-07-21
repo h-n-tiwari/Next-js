3b99950fac07b2b3dd72b05b44b8559289187d26
h-n-tiwari <pmgc590@gmail.com>
2026-07-21 15:22:39 +0530

    Testing the Carousel in Server Components

--- rendering-demo/src/app/server-route/page.tsx
+++ rendering-demo/src/app/server-route/page.tsx
@@ -1,9 +1,38 @@
-import { serverSideFunction } from "@/utils/server-utils";
+// import { serverSideFunction } from "@/utils/server-utils";
 
-export default function ServerRoutePage() {
-    const result = serverSideFunction();
-    return <h1>Server Route {result}</h1>
-       
+// export default function ServerRoutePage() {
+//     const result = serverSideFunction();
+//     return <h1>Server Route {result}</h1>
+
+// }
+
+// THIRD PARTY PACKAGES
+
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export default function ClientRoutePage() {
+    const settings = {
+        dots: true,
+    };
+    return (
+        <div className="image-slider-container">
+            <Slider {...settings}>
+                <div>
+                    <img src="https://picsum.photos/400/200" />
+                </div>
+                <div>
+                    <img src="https://picsum.photos/400/200" />
+                </div>
+                <div>
+                    <img src="https://picsum.photos/400/200" />
+                </div>
+                <div>
+                    <img src="https://picsum.photos/400/200" />
+                </div>
+            </Slider>
+        </div>
+    );
 }
-
-
