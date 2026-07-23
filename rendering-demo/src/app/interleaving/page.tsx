a7cd893792b72d2d6dda4705fc473963efbfb5d8
h-n-tiwari <pmgc590@gmail.com>
2026-07-23 11:26:42 +0530

    Client Component Inside a Server Component

--- rendering-demo/src/app/interleaving/page.tsx
+++ rendering-demo/src/app/interleaving/page.tsx
@@ -1,12 +1,12 @@
-// import { ServerComponentOne } from "@/components/server-component-one";
-import { ClientComponentOne } from "@/components/client-component-one";
+import { ServerComponentOne } from "@/components/server-component-one";
+
 
 export default function InterLeavingPage() {
     return (
         <>
             <h1>InterLeaving Page</h1>
-            {/* <ServerComponentOne /> */}
-            <ClientComponentOne />
+            <ServerComponentOne />
+
         </>
     )
 }
