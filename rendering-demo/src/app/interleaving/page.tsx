4188043875d11a95eb7982b52d41eea4cdbe87d6
h-n-tiwari <pmgc590@gmail.com>
2026-07-23 10:28:33 +0530

    Server Component Inside Another Server Component

--- rendering-demo/src/app/interleaving/page.tsx
+++ rendering-demo/src/app/interleaving/page.tsx
@@ -1,7 +1,10 @@
+import { ServerComponentOne } from "@/components/server-component-one"
+
 export default function InterLeavingPage() {
     return (
         <>
             <h1>InterLeaving Page</h1>
+            <ServerComponentOne />
         </>
     )
 }
