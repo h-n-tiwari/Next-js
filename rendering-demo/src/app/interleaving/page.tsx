8d3cf6ca74b020e3872b5d543797fc57ec377f3f
h-n-tiwari <pmgc590@gmail.com>
2026-07-23 11:21:22 +0530

    Client Component Inside Another Client Component

--- rendering-demo/src/app/interleaving/page.tsx
+++ rendering-demo/src/app/interleaving/page.tsx
@@ -1,10 +1,12 @@
-import { ServerComponentOne } from "@/components/server-component-one"
+// import { ServerComponentOne } from "@/components/server-component-one";
+import { ClientComponentOne } from "@/components/client-component-one";
 
 export default function InterLeavingPage() {
     return (
         <>
             <h1>InterLeaving Page</h1>
-            <ServerComponentOne />
+            {/* <ServerComponentOne /> */}
+            <ClientComponentOne />
         </>
     )
 }
--- rendering-demo/src/components/client-component-one.tsx
+++ rendering-demo/src/components/client-component-one.tsx
@@ -1,8 +1,14 @@
 "use client";
 
 import { useState } from "react";
+import { ClientComponentTwo } from "./client-component-two";
 
 export const ClientComponentOne = () => {
     const [name, setName] = useState("BatMan");
-    return <h1>Client Component One</h1>
+    return (
+        <>
+            <h1>Client Component One</h1>
+            <ClientComponentTwo />
+        </>
+    );
 }
