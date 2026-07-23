9a24ff3a5f0b18f738bf70cef60f60758ae17c86
h-n-tiwari <pmgc590@gmail.com>
2026-07-23 05:40:43 +0530

    1.Implement State in Navbar 2. Optimizing Client Component Placement

--- rendering-demo/src/components/nav-search.tsx
+++ rendering-demo/src/components/nav-search.tsx
@@ -1,6 +1,12 @@
 "use client"
 
+import { useState } from "react";
+
+
 export const NavSearch = () => {
     console.log(`NavSearch Rendered`);
+
+    const [search, setSearch] = useState("");
+
     return <div>Nav Search Input</div>
 }
--- rendering-demo/src/components/navbar.tsx
+++ rendering-demo/src/components/navbar.tsx
@@ -1,6 +1,3 @@
-"use client"
-
-import { useState } from "react";
 import { NavLinks } from "./nav-links";
 import { NavSearch } from "./nav-search";
 
@@ -9,7 +6,6 @@ import { NavSearch } from "./nav-search";
 export const NavBar = () => {
     console.log(`NavBar Rendered`);
 
-    const [search, setSearch] = useState("");
 
     return (
         <div>
