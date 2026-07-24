d05b265d3d1fb245a83c5715c0a2825f92bd39fd
h-n-tiwari <pmgc590@gmail.com>
2026-07-24 15:06:49 +0530

    Fetching Data With useEffect

--- data-fetching-demo/src/app/users-client/page.tsx
+++ data-fetching-demo/src/app/users-client/page.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type User = {
     id: number;
@@ -12,5 +12,51 @@ export default function UserClient() {
     const [users, setUsers] = useState<User[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
+
+    "use client";
+
+    import { useEffect, useState } from "react";
+
+    type User = {
+        id: number;
+        name: string;
+        username: string;
+        email: string;
+        phone: string;
+    };
+
+    export default function UserClient() {
+        const [users, setUsers] = useState<User[]>([]);
+        const [loading, setLoading] = useState(true);
+        const [error, setError] = useState("");
+
+        useEffect(() => {
+            async function fetchUsers() {
+                try {
+                    const response = await fetch(
+                        "https://jsonplaceholder.typicode.com/users",
+                    );
+
+                    if (!response.ok) {
+                        throw new Error("Failed to fetch users");
+                    }
+
+                    const data: User[] = await response.json();
+                    setUsers(data);
+                } catch (err) {
+                    if (err instanceof Error) {
+                        setError(err.message);
+                    } else {
+                        setError("An unknown error occurred");
+                    }
+                } finally {
+                    setLoading(false);
+                }
+            }
+
+            fetchUsers();
+        }, []);
+
+      
 
 }
