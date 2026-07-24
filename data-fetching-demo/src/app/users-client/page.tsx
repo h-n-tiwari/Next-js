b4436c7e75aa80f66d4f93f402a51107563751b3
h-n-tiwari <pmgc590@gmail.com>
2026-07-24 15:10:00 +0530

    Handling UI States

--- data-fetching-demo/src/app/users-client/page.tsx
+++ data-fetching-demo/src/app/users-client/page.tsx
@@ -13,50 +13,37 @@ export default function UserClient() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
 
-    "use client";
+    useEffect(() => {
+        async function fetchUsers() {
+            try {
+                const response = await fetch(
+                    "https://jsonplaceholder.typicode.com/users",
+                );
 
-    import { useEffect, useState } from "react";
-
-    type User = {
-        id: number;
-        name: string;
-        username: string;
-        email: string;
-        phone: string;
-    };
-
-    export default function UserClient() {
-        const [users, setUsers] = useState<User[]>([]);
-        const [loading, setLoading] = useState(true);
-        const [error, setError] = useState("");
-
-        useEffect(() => {
-            async function fetchUsers() {
-                try {
-                    const response = await fetch(
-                        "https://jsonplaceholder.typicode.com/users",
-                    );
-
-                    if (!response.ok) {
-                        throw new Error("Failed to fetch users");
-                    }
-
-                    const data: User[] = await response.json();
-                    setUsers(data);
-                } catch (err) {
-                    if (err instanceof Error) {
-                        setError(err.message);
-                    } else {
-                        setError("An unknown error occurred");
-                    }
-                } finally {
-                    setLoading(false);
+                if (!response.ok) {
+                    throw new Error("Failed to fetch users");
                 }
+
+                const data: User[] = await response.json();
+                setUsers(data);
+            } catch (err) {
+                if (err instanceof Error) {
+                    setError(err.message);
+                } else {
+                    setError("An unknown error occurred");
+                }
+            } finally {
+                setLoading(false);
             }
+        }
 
-            fetchUsers();
-        }, []);
+        fetchUsers();
+    }, []);
 
-      
+    if (loading) return <div>Loading...</div>;
+
+    if (error) return <div>{error}</div>;
+
+    
 
 }
