70dfbdbe6d7d8d3eb94362c2eb057450c98f03e3
h-n-tiwari <pmgc590@gmail.com>
2026-07-24 15:02:45 +0530

    Creating the User Clien Component

---
+++ data-fetching-demo/src/app/users-client/page.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+type User = {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+}
+
+export default function UserClient() {
+    const [users, setUsers] = userState<User[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+
+}
