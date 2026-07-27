f7f80532fdedc63f5de48a1944463d30bb9a2167
h-n-tiwari <pmgc590@gmail.com>
2026-07-27 15:29:29 +0530

    Testing

--- data-fetching-demo/src/app/users-server/page.tsx
+++ data-fetching-demo/src/app/users-server/page.tsx
@@ -10,7 +10,7 @@ type User = {
 
 export default async function UserServer() {
     await new Promise((resolve) => setTimeout(resolve, 2000))
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const response = await fetch("https://jsonplaceholder.typicode.com/users123");
     const users: User[] = await response.json();
     console.log(users);
 
