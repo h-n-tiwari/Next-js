0d9a211965dc9e12fce0324cf07cdd733f46bbb2
h-n-tiwari <pmgc590@gmail.com>
2026-07-29 19:46:28 +0530

    Parallel Data Fetching - Creating the User Profile Page

---
+++ data-fetching-demo/src/app/user-parallel/[id]/page.tsx
@@ -0,0 +1,16 @@
+type Post = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+type Album = {
+    userId: number;
+    id: number;
+    title: string;
+}
+
+export default async function UserProfile() {
+    
+}
