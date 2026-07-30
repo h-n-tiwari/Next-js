f1e97a313ad699c11ade2db5707d949b35ea1248
h-n-tiwari <pmgc590@gmail.com>
2026-07-30 11:49:06 +0530

    Implementing Parallel Data Fetching

--- data-fetching-demo/src/app/user-parallel/[id]/page.tsx
+++ data-fetching-demo/src/app/user-parallel/[id]/page.tsx
@@ -11,6 +11,33 @@ type Album = {
     title: string;
 }
 
-export default async function UserProfile() {
+async function getUserPosts(userId: string) {
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+    );
+
+    return res.json();
+}
+
+async function getUserAlbums(userId: string) {
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/albums?userId=${userId}`,
+    );
+
+    return res.json();
+
+}
+
+export default async function UserProfile({ 
+    params,
+}:  {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params;
+
+    const postData = getUserPosts(id);
+    const albumsData = getUserAlbums(id);
+
+    const [posts, albums] = await Promise.all([postData, albumsData]);
     
 }
