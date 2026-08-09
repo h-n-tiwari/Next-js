65a515b0dc4bd0135f998742b3b1f8fae1ce9ddb
h-n-tiwari <pmgc590@gmail.com>
2026-08-09 15:08:31 +0530

    Installed and Implement Driver Adapters

--- data-fetching-demo/src/prisma-db.ts
+++ data-fetching-demo/src/prisma-db.ts
@@ -1,5 +1,11 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./dev.db",
+});
+
+const prisma = new PrismaClient({ adapter });
 
 const seedProducts = async () => {
   const count = await prisma.product.count();
