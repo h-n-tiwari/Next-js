366bce7d36d1d29e3a4b5ff2f381d958f5626b5c
h-n-tiwari <pmgc590@gmail.com>
2026-08-18 22:14:08 +0530

    Handling Typescript Errors

--- data-fetching-demo/src/app/products-db/[id]/page.tsx
+++ data-fetching-demo/src/app/products-db/[id]/page.tsx
@@ -1,6 +1,7 @@
-// import { Submit } from "@/components/submit";
 import { getProduct } from "@/prisma-db";
 import EditProductForm from "./product-edit-form";
+import type { Product } from "@/app/products-db/page";
+import { notFound } from "next/navigation";
 
 export default async function EditProductPage({
   params,
@@ -8,7 +9,12 @@ export default async function EditProductPage({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const product = await getProduct(parseInt(id));
+
+  const product: Product | null = await getProduct(parseInt(id));
+
+  if (!product) {
+    notFound();
+  }
 
   return <EditProductForm product={product} />;
 }
