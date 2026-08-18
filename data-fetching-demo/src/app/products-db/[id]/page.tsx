c267c213b0fa01f371c6dd326b7f01d609d2e514
h-n-tiwari <pmgc590@gmail.com>
2026-08-18 05:55:30 +0530

    Fetching Product Data

--- data-fetching-demo/src/app/products-db/[id]/page.tsx
+++ data-fetching-demo/src/app/products-db/[id]/page.tsx
@@ -4,7 +4,16 @@
 import { useActionState } from "react";
 import { FormState, createProduct } from "@/actions/products";
 
-export default function EditProductPage() {
+export default function EditProductPage({
+  params,
+}: {
+    params: Promise<{ id: string }>;
+  }) {
+
+  const { id } = await params;
+  const product = await getProduct(parseInt(id));
+
+
   const initialState: FormState = { errors: {} };
 
   const [state, formAction, isPending] = useActionState(
