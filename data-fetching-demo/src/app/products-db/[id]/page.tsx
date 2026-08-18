d5fa38e7d28bb4f6f8ade1451caa370b5d31bbbb
h-n-tiwari <pmgc590@gmail.com>
2026-08-18 06:06:31 +0530

    Splitting Logic into Components

--- data-fetching-demo/src/app/products-db/[id]/page.tsx
+++ data-fetching-demo/src/app/products-db/[id]/page.tsx
@@ -1,8 +1,6 @@
-"use client";    // Before using directive getting error and after using directive again hitting error.
-
 // import { Submit } from "@/components/submit";
 import { useActionState } from "react";
-import { FormState, createProduct } from "@/actions/products";
+import EditProductForm from "./product-edit-form";
 
 export default function EditProductPage({
   params,
@@ -13,69 +11,5 @@ export default function EditProductPage({
   const { id } = await params;
   const product = await getProduct(parseInt(id));
 
-
-  const initialState: FormState = { errors: {} };
-
-  const [state, formAction, isPending] = useActionState(
-    createProduct,
-    initialState,
-  );
-
-  return (
-    <form action={formAction} className="p-4 space-y-4 max-w-96">
-      <div>
-        <label className="text-white">
-          Title
-          <input
-            type="text"
-            className="block w-full p-2 text-black border rounded bg-white"
-            name="title"
-          />
-        </label>
-
-        {state.errors.title && (
-          <p className="text-red-500">{state.errors.title}</p>
-        )}
-      </div>
-
-      <div>
-        <label className="text-white">
-          Price
-          <input
-            type="number"
-            className="block w-full p-2 text-black border rounded bg-white"
-            name="price"
-          />
-        </label>
-
-        {state.errors.price && (
-          <p className="text-red-500">{state.errors.price}</p>
-        )}
-      </div>
-
-      <div>
-        <label className="text-white">
-          Description
-          <textarea
-            className="block w-full p-2 text-black border rounded bg-white"
-            name="description"
-          />
-        </label>
-
-        {state.errors.description && (
-          <p className="text-red-500">{state.errors.description}</p>
-        )}
-      </div>
-
-      <button
-        type="submit"
-        className="block w-full p-2 mt-4  text-white  bg-blue-500 rounded disabled:bg-gray-500"
-        disabled={isPending}
-      >
-        Submit
-      </button>
-
-      {/*<Submit />*/}
-    </form>
-  );
+  return <EditProductForm />
 }
